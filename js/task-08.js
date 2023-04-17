d658affb3191007eec781d8fa7adf069fd40db06
N0T4 <118122479+N0T4@users.noreply.github.com>
2023-04-17 11:42:06 +0100

    1

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,30 @@
+const form = document.querySelector('.login-form')
+
+form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+event.preventDefault()
+
+const email = event.currentTarget.elements.email.value
+const password = event.currentTarget.elements.password.value
+
+if (!email || !password) {
+alert('Будь-ласка заповніть форму')
+return
+}
+
+const formData = {
+email: email,
+password: password
+}
+
+console.log(formData)
+
+event.currentTarget.reset()
+}
+
+
+
+
+
 
